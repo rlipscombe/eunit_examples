fd2bce41b46bb561ea0d7c9a8c8eda8bbb3a5110
Roger Lipscombe <roger@differentpla.net>
2023-04-01 16:13:23 +0100

    Simplify the foreach test

--- test/simple_foreach_test.erl
+++ test/simple_foreach_test.erl
@@ -4,9 +4,19 @@
 simple_foreach_test_() ->
     % 'foreach' sets up a fixture for each of the specified tests.
     {foreach, fun setup/0, fun cleanup/1, [
-        fun() -> ?assert(true) end,
-        ?_assert(true)
+        fun some_test/0,
+        fun another_test/0
     ]}.
 
-setup() -> ok.
-cleanup(_) -> ok.
+setup() ->
+    {ok, Pid} = some_server:start_link(),
+    Pid.
+
+cleanup(Pid) ->
+    % The result from suite_setup is passed as the argument to suite_cleanup.
+    unlink(Pid),    % ...so we don't also get killed
+    exit(Pid, kill),
+    ok.
+
+some_test() -> ?assert(true).
+another_test() -> ?assert(true).
