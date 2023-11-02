1277d4ce3f1bfffafbb7b421e928329e1e881d7e
Roger Lipscombe <roger@differentpla.net>
2023-11-02 09:42:54 +0000

    Add add/divide functions to calculator

--- src/calculator.erl
+++ src/calculator.erl
@@ -1,11 +1,15 @@
 -module(calculator).
 -export([
+    add/2,
     subtract/2,
-    multiply/2
+    multiply/2,
+    divide/2
 ]).
 
+add(L, R) -> L + R.
 subtract(L, R) -> L - R.
 multiply(L, R) -> L * R.
+divide(L, R) -> L / R.
 
 %% You can keep tests next to the code they're testing. There are a few times this makes sense:
 %% - You want to test a private function. Try to avoid this.
