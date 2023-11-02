27c89e9a3fdf2f36282470bad7c795fbe8be2ff9
Roger Lipscombe <roger@differentpla.net>
2023-11-02 09:16:19 +0000

    Demonstrate in-source tests

--- src/calculator.erl
+++ src/calculator.erl
@@ -1,4 +1,18 @@
 -module(calculator).
--export([multiply/2]).
+-export([
+    subtract/2,
+    multiply/2
+]).
 
+subtract(L, R) -> L - R.
 multiply(L, R) -> L * R.
+
+%% You can keep tests next to the code they're testing. There are a few times this makes sense:
+%% - You want to test a private function. Try to avoid this.
+%% - Your application is simple enough that having the tests next to the code is better. This is a matter of opinion.
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+subtract_test() ->
+    ?assertEqual(12, subtract(18, 6)).
+-endif.
