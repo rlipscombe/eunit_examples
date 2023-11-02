3f0c3e1620cd9763d549a8191ccdd15e010b7625
Roger Lipscombe <roger@differentpla.net>
2023-11-02 08:56:34 +0000

    Delete 'meaning' stuff
    I'm going to build this back up differently.

--- src/meaning.erl
+++
@@ -1,4 +0,0 @@
--module(meaning).
--export([meaning_of/1]).
-
-meaning_of([life, universe, everything]) -> 42.
--- test/meaning_test.erl
+++
@@ -1,5 +0,0 @@
--module(meaning_test).
--include_lib("eunit/include/eunit.hrl").
-
-the_test() ->
-    ?assertEqual(42, meaning:meaning_of([life, universe, everything])).
